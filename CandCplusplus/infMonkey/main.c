f26bf9c4536a561c51f90cee03c94265957dcce1
mooreBrendan <moore749@purdue.edu>
2019-04-28 22:46:48 -0400

    made an infinite monkey program to print all possible strings of a given length

---
+++ CandCplusplus/infMonkey/main.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+static void infMonkey(char*, int, int);
+static void printArray(char*, int);
+
+int main(int argc, char** argv){
+	if(argc < 2){
+		return(EXIT_FAILURE);
+	}
+	int size = strtol(argv[1], NULL, 10); 
+	char* strng = malloc(sizeof(char) * size);
+	if(strng == NULL){
+		return(EXIT_FAILURE);
+	}
+	infMonkey(strng, 0, size);
+
+	free(strng);
+	return(EXIT_SUCCESS);
+}
+
+
+
+
+static void infMonkey(char* strng, int count, int size){
+	int i;
+	if(count >= size || count < 0 || size <= 0){
+		return;
+	}
+	if(count + 1 == size){
+		for(i = 0; i< 26;i++){
+			strng[count] = 65+ i;
+			printArray(strng, size);
+		}
+	}else{
+		for(i = 0; i< 26; i++){
+			strng[count] = 65 + i;
+			infMonkey(strng, count + 1, size);
+		}
+	}
+
+
+}
+
+static void printArray(char* strng, int size){
+	int i;
+	for(i = 0; i< size; i++){
+		printf("%c",strng[i]); 
+	}
+	printf("\n");
+}
