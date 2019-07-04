6e79d52cb0e8a4c35090149e52daf6b78b3efb00
mooreBrendan <moore.brendanm@comcast.net>
2019-07-03 20:29:55 -0400

    rewrite of calculate, needs testing and precedence

--- CandCplusplus/calc/main.c
+++ CandCplusplus/calc/main.c
@@ -12,62 +12,75 @@ long int raise(long int base, long int power){
 	}
 }
 
-int calculate(FILE** fp, long int* returnVal){
-	long int calc;
-	long int store;
-	char temp = ' ';
-	while(temp != '\n' && !feof(*fp)){
-		calc = 0;
-		store = 0;
-		temp = ' ';
-		if(!fscanf(*fp, "%ld", &calc)){
-			fscanf(*fp, "%c",&temp);
-			if(temp != '('){
-				return(-1);
-			}else{
-				if(calculate(fp, &store)==-1){
-					return(-1);
-				}else{
-					*returnVal += store;
-				}
+long int calc(long int calc1, short int state, long int calc2){
+	switch(state){
+		case 0:		return(0);
+		case 1:		return(calc1 + calc2);
+		case 2:		return(calc1 - calc2);
+		case 3:		return(calc1 * calc2);
+		case 4:		return(calc1 / calc2);
+		case 5:		return(raise(calc1, calc2));
+		default:	return(-1);
+	}
+}
+
+short int conv(char input){
+	switch(input){
+		case '+': return(1);
+		case '-': return(2);
+		case '*': 
+		case 'x':
+		case 'X': return(3);
+		case '/': return(4);
+		case '^': return(5);
+		default: return(-1);
+	}
+}
+
+long int calculate(FILE** fp, long int* finalVal){
+	long int calc1;				//first operator
+	long int calc2;				//second operator
+	short int state;			//operand value
+	short int returnVal;	//return vlaue
+	char temp;						//store variable for parenthesis
+	
+	//get first operator
+	if(fscanf(*fp,"%ld",&calc1) != 1){ //try and get first operator
+		do{
+			temp = 0;
+			if( fscanf(*fp,"%c",&temp) == 1 && temp == '('){ //check if open par.
+				state = calculate(fp, &calc1); //get operator and operand
+			}else if(temp != ' '){ //if not par
+				return(-1); //return error
 			}
+		}while( temp != '(' && temp != ' '); //move past spaces
+	} else{
+		//get operand
+		do{
+			temp = ' ';
+			fscanf(*fp, "%c",&temp); 
+		}while(temp == ' ');
+		if(temp == '('){
+			state = calculate(fp, &calc2);
+			calc1 *= calc2;
 		}else{
-			do{
-				fscanf(*fp, "%c", &temp);
-			}while(!feof(*fp) && temp == ' ');
-		}
-		if(temp != '\n' && !feof(*fp)){
-			if(temp == '+'){
-				*returnVal += calc;
-			}else if(temp == '-'){
-				*returnVal -= calc;
-			}else if(temp == '*' || temp == '('){
-				if(calculate(fp, &store) != -1){
-					*returnVal += calc * store;
-				}else{
-					return(-1);
-				}
-			}else if(temp == '/'){
-				if(calculate(fp, &store) != -1){
-					*returnVal += calc / store;
-				}else{
-					return(-1);
-				}
-			}else if(temp == ')'){
-				return(0);
-			}else if(temp == '^'){
-				if(calculate(fp, &store) != -1){
-					*returnVal += raise(calc, store);
-				}else{
-					return(-1);
-				}
-			}else{
-				return(-1);
-			}
+			state = conv(temp);
 		}
 	}
-	*returnVal += calc;
-	return(0);
+
+	//read in rest of file
+	do{
+		//get second operator
+		returnVal = calculate(fp, &calc2);
+	
+		//perform operation
+		calc1 = calc(calc1, state, calc2);
+	}while(returnVal >0);
+
+	*finalVal = calc1;
+
+	//return if additional operations are needed
+	return(returnVal);
 }
 
 int main(int argc, char** argv){
