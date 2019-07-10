784cf5e544e7008f734575b76999870644c069d4
mooreBrendan <moore.brendanm@comcast.net>
2019-07-09 22:19:37 -0400

    initial work towards changing bases

--- python/clockBase/clock.py
+++ python/clockBase/clock.py
@@ -2,8 +2,28 @@ import time
 
 class clock():
 	def __init__(self,base):
-		self.base = base
-	def update(self):
+		base = int(base)
+		if(base < 2):
+			self.base = 2
+		elif(base >16):
+			self.base = 16
+		else:
+			self.base = base
+	def update(self, base):
 		self.hour = time.localtime().tm_hour
 		self.min = time.localtime().tm_min
 		self.sec = time.localtime().tm_sec
+		self.base = base
+	def convert(self):
+		self.baseHour = []
+		self.baseMin = []
+		self.baseSec = []
+		while self.hour >0:
+			self.baseHour.prepend(self.hour %base)
+			self.hour = (self.hour - (self.hour %base))/base
+		while self.min >0:
+			self.baseMin.prepepend(self.min %base)
+			self.min = (self.min - (self.min %base))/base
+		while self.sec >0:
+			self.baseSec.prepend(self.sec %base)
+			self.sec = (self.sec - (self.sec %base))/base
--- python/clockBase/main.py
+++ python/clockBase/main.py
@@ -1,4 +1,5 @@
 import clock
+import time
 
 base = 10
 
@@ -6,7 +7,10 @@ clk = clock.clock(base)
 
 try:
 	while 1 == 1:
-		clk.update()
-		print(str(clk.hour)+":"+str(clk.min)+":"+str(clk.sec))
+		clk.update(base)
+		print(str(clk.base) +":\t"+ str(clk.hour)+":"+str(clk.min)+":"+str(clk.sec))
+		clk.convert()
+		print(str(clk.base) +":\t"+ str(clk.baseHour)+":"+str(clk.baseMin)+":"+str(clk.baseSec))
+		time.sleep(1)
 except:
 	print("exiting")
