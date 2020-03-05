6f64af0592f9295578b36da20ef73e593040cf64
mooreBrendan <moore.brendanm@comcast.net>
2020-03-05 13:49:03 -0500

    refactored debug code to accept multiple lengths of code

--- CandCplusplus/AES/main.c
+++ CandCplusplus/AES/main.c
@@ -13,7 +13,6 @@ int main(int argc,char** argv){
 		}
 	}*/
 
-/*
 	if(argc < 2){
 		return(EXIT_FAILURE);
 	}
@@ -21,7 +20,7 @@ int main(int argc,char** argv){
 	
 	
 	fclose(fin);
-	*/
+
 	return(EXIT_SUCCESS);
 }
 
