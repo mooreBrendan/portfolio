2c4bb2893b477585b06c8da9103e96b8e42299da
mooreBrendan <moore.brendanm@comcast.net>
2020-03-23 00:15:09 -0400

    added comments more skeleton code, need to reverse board object heirarchy, need to change player resource management

--- python/catan/catan.py
+++ python/catan/catan.py
@@ -1,10 +1,30 @@
+# imports
+############################################
 from enum import Enum
 
+
+# Global variables
+############################################
+winValue = 10
+
+############################################
+# Function:
+#	Inputs:
+# Returns:
+# Purpose:
+############################################
 def diceRoll:
 	dice1 = random(1,6)
 	dice2 = random(1,6)
 	return(dice1 + dice2)
 
+
+############################################
+# Function:
+#	Inputs:
+# Returns:
+# Purpose:
+############################################
 class Terrains(Enum):
 	Mountains = "ore"
 	Hills = "brick"
@@ -13,8 +33,15 @@ class Terrains(Enum):
 	Fields = "wheat"
 	Desert = "none"
 
+
+############################################
+# Class:
+#	Inputs:
+# Methods:
+# Purpose:
+############################################
 class Player:
-	def __init__(self):
+	def __init__(self,token):
 		self.__cards = []
 		self.__ore = 0
 		self.__brick = 0
@@ -24,7 +51,7 @@ class Player:
 		self.__nodes = []
 		self.__edges = []
 		self.__victoryPoints = 0
-
+		self.sessionToken = token
 	def cullCards(self):
 		if len(self.__cards) > 7:
 			#choose cards to remove
@@ -48,16 +75,65 @@ class Player:
 				print("Error: choice not recognized")
 
 	def getChoice(self):
-		return "end"
+		choice = "end"
+		##########
+		return choice
+
+	def checkWin(self):
+		return (self.__victoryPoints >= winValue) 
 
 
+############################################
+# Class:
+#	Inputs:
+# Methods:
+# Purpose:
+############################################
 class Node:
 	def __init__(self,edges):
 		self.__owner = None
 		self.__edges = edges
 
+
+############################################
+# Class:
+#	Inputs:
+# Methods:
+# Purpose:
+############################################
 class Edge:
 	def __init__(self,tiles):
 		self.__owner = None
 		self.__tiles = tiles
 
+	def setOwner(self,player):
+		self.__owner = player
+	
+	def giveResource(self,resource):
+		if self.owner != None:
+			self.owner[resource] += 1
+
+
+
+############################################
+# Class:
+#	Inputs:
+# Methods:
+# Purpose:
+############################################
+class Tile:
+	def __init__(self,number,terrain):
+		self.number = number
+		self.terrain = terrain
+		self.resource = Terrains(terrain)
+		self.edges = self.genEdges()
+
+	def genEdges(self):
+		edges = []
+		for i in range(6):
+			edges.append(Edge([]))
+		return edges
+
+	def giveResources(self):
+		for i in self.edges:
+			i.giveResource(self.resource)
--- python/catan/main.py
+++ python/catan/main.py
@@ -1,21 +1,58 @@
+# imports
+############################################
 from catan import *
 
+
+
+############################################
+# Function: getNumPlayers
+#	Inputs: none
+# Returns: the number of players
+# Purpose: takes the input number of players
+############################################
 def getNumPlayers:
 	return 4
 
+
+############################################
+# Function:
+#	Inputs:
+# Returns:
+# Purpose:
+############################################
+def getTokens(numPlayers):
+	tokens = []
+	############
+	return tokens
+
+
+############################################
+# Function:
+#	Inputs:
+# Returns:
+# Purpose:
+############################################
 def getPlayers:
 	numPlayers = getNumPlayers()
+	tokens = getTokens(numPlayers)
 	players = []
-	for i in range(numPlayers):
-		players.append(Player())
+	for i in tokens:
+		players.append(Player(i))
 	return(players)
 
+############################################
+# Function:
+#	Inputs:
+# Returns:
+# Purpose:
+############################################
 def getTiles:
 	tiles = []
 	
 	return tiles
 	
-
+# MAIN
+############################################
 if __name__ == "__main__":
 	print("running")
 
@@ -24,9 +61,23 @@ if __name__ == "__main__":
 	finished = False
 	while not finished:
 		for i in players:
-			roll = diceRoll()
-			if roll == 7:
-				for j in players:
-					j.cullCards()
-				i.moveRobber
-			i.buildTrade()
+			if not finished:
+				roll = diceRoll()
+				if roll == 7:
+					for j in players:
+						j.cullCards()
+					i.moveRobber
+				i.buildTrade()
+				if i.checkWin():
+					print("Game Over, {} won".format(i.color))
+					finished = True
+
+
+
+############################################
+# Function:
+#	Inputs:
+# Returns:
+# Purpose:
+############################################
+
