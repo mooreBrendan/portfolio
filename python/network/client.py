13ea2259d5676ad69215f833112b7e425b87ce25
mooreBrendan <moore.brendanm@comcast.net>
2020-05-28 00:52:44 -0400

    added initial connection for client to authentication

--- python/network/client.py
+++ python/network/client.py
@@ -8,12 +8,26 @@ class client:
 	def __sendCommand(self, command):
 		print(command)
 
+	def __authenticate(self, portNum):
+		return 0
+
 	def run()
-		port = input("server port num:")
-		try:
-			response = self.__sendCommand(int(port),"auth")
-		except:
-			print("invalid port")
+		#connect to authentication server
+		response = 0
+		while response != 1:
+			port = input("server port num: ")
+			try:
+				response = self.__authenticate(int(port))
+			except:
+				print("invalid port")
+		self.authServer = port
+
+		#command prompt
+		while 1 == 1:
+			command = input("cmd: ")
+			cmd = self.__validateCmd(command)
+			if cmd != ""
+				self.__sendCommand(cmd)
 
 if __name__ == "__main__":
 	cl = client(sys.argv[1])
