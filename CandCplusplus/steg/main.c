d8b70c2fdf568922da5a6ad9fa01646da36939da
mooreBrendan <moore.brendanm@comcast.net>
2020-05-28 20:05:06 -0400

    refactored main to be cleaner

--- CandCplusplus/steg/main.c
+++ CandCplusplus/steg/main.c
@@ -3,21 +3,17 @@
 int main(int argc, char** argv){
 	//initialize
 	int mode = checkInput(argc, argv[1]);
-	if(mode == -1){
-		return(EXIT_FAILURE);
-	}else if(mode == 0){
+	if(mode == 0){
 		return(EXIT_SUCCESS);
-	}
-
-	//get password
-	unsigned int seed = promptPassword();
-	srand(seed);
-	seed = 0;
-
-	//run program
-	if(mode == 1){ //read from message (decode)
+	}else if(mode == 1){ //read from message (decode)
+		unsigned int seed = promptPassword(); //get password
+		srand(seed);
+		seed = 0;  //reset seed in memmory
 		decode(argv[2], argv[3]);
 	}else if(mode == 2){ //add message (encode)
+		unsigned int seed = promptPassword(); //get password
+		srand(seed);
+		seed = 0; //reset seed im memmory
 		encode(argv[2], argv[3], argv[4]);
 	}else{
 		return(EXIT_FAILURE);
